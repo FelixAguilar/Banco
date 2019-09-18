249bebd74b0ab94dab439cfd1bae5873a75028d8
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-09-18 15:05:01 +0200

    Subida clase cliente

---
+++ src/megabanco/Cliente.java
@@ -0,0 +1,64 @@
+package megabanco;
+
+import megabanco.Cuenta;
+
+/**
+ *
+ * @author felix
+ */
+public class Cliente {
+    
+    private String nombre;
+    private Cuenta[] cuentas;
+
+    public Cliente(String nombre, int saldo, int idCuenta) {
+        this.nombre = nombre;
+        this.cuentas = new Cuenta[1];
+        cuentas[0] = new Cuenta(saldo, idCuenta);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Cuenta[] getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(Cuenta[] cuentas) {
+        this.cuentas = cuentas;
+    }
+    
+    private void redimensionarArray(boolean eliminar, int indice){
+        
+        if(eliminar){
+            
+            Cuenta[] temporal = new Cuenta[this.cuentas.length-1];
+            
+            for (int i = 0; i < temporal.length; i++) {
+                if (indice < i) {
+                    temporal[i] = this.cuentas[i];
+                }else{
+                    temporal[i] = this.cuentas[i+1];
+                }
+            }
+            
+            this.cuentas = temporal;
+        }
+        
+        else{
+            Cuenta[] temporal = new Cuenta[this.cuentas.length+indice];
+            
+            for (int i = 0; i < this.cuentas.length; i++) { 
+                temporal[i] = this.cuentas[i];
+            }
+            
+            this.cuentas = temporal;
+        }
+        
+    }
+    
+    
+    
+    
+}
