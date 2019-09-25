1e0a7f8bed30fa0b715fff2c66dc8aa65e8276b4
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-09-25 17:48:48 +0200

    error al subir el archivo

--- src/megabanco/MetodosyDatos.java
+++ src/megabanco/MetodosyDatos.java
@@ -1,110 +1,205 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package megabanco;
 
 /**
- *
- * @author Adrián & Álvaro
+ * Clase que contiene todos los métodos para el funcionamiento del banco.
+ * 
+ @author Felix Aguilar Ferrer, Adrian Bennassar Polzin, Alvaro Bueno Lopez.
  */
 public class MetodosyDatos {
 
+    // Lista clientes.
     private Cliente[] clientes = new Cliente[0];
-    private int posCli = 0;
 
-    public MetodosyDatos() {
-    };
-    
-    
-    public Cliente buscarCuenta(int id) throws ExcepcionCuentaNoExiste {
-        Cliente cli = null;
-        boolean endBucle = false;
-        for (int i = 0; (i < clientes.length) && (endBucle); i++) {
-            for (int j = 0; j < clientes[j].getCuentas().length; j++) {
-                if (clientes[i].getCuenta(j).getID() == id) {
-                    cli = new Cliente(clientes[i].getNombre(), clientes[i].getCuenta(j).getSaldo(), clientes[i].getCuenta(j).getID());
-                    endBucle = true;
-                }
-
-            }
-
-        }
-        if (cli == null) {
-            throw new ExcepcionCuentaNoExiste();
-        }
-        return cli;
-    }
-
-
-    /*Método para comprobar si un cliente pasado por parámetro ya existe en el 
-    array de clientes*/
-    public boolean clienteExistente(String nom) {
-        boolean existe = false;
-        for (int i = 0; i < posCli; i++) {
-            if (nom.equals(clientes[i].getNombre())) {
-                existe = true;
-            }
-        }
-        return existe;
-
-    }
-
-    /*Método para comprobar hay algún cliente dentro del array de clientes*/
-    public boolean hayClientes() {
-        boolean hayCli = false;
-        if (clientes[0] != null) {
-            hayCli = true;
-
-        }
-        return hayCli;
-    }
-
-    /*Método para crear un nuevo cliente*/
-    public Cliente nuevoCliente(String nom, int sal) {
-        int idIni = posCli;
-        Cliente cli = new Cliente(nom, sal, idIni);
-
+    /**
+     * Método para añadir un cliente o cuenta a la lista de clientes.
+     * 
+     * @param nom
+     * @param sal
+     * @return
+     */
+    public String apartado1(String nom, int sal){
+        String s="";
+        int ID = generadorID();
         try {
+            int ind = buscarCliente(nom);
+            clientes[ind].redimensionarArrayCuentas(false, 1);
+            clientes[ind].setCuenta(clientes[ind].getCuentas().length - 1, new Cuenta(ID, sal));
+            s += "Cuenta añadida a cliente existente.\n";
+            s += "Nombre cuenta: " + clientes[ind].getNombre() + ".\n";
+            s += "ID Cuenta: " + clientes[ind].getCuentas()[clientes[ind].getCuentas().length-1].getID() + ".\n";
+            s += "Saldo: " + clientes[ind].getCuentas()[clientes[ind].getCuentas().length-1].getSaldo() + ".\n";
+            
+        } catch (ExcepcionClienteNoEncontrado ex) {
             redimensionarArrayCli(false, 1);
-            if (hayClientes()) {
-                if (clienteExistente(nom)) {
-                    throw new ExceptionClienteExistente();
+            clientes[clientes.length - 1] = new Cliente(nom, sal,ID );
+            s += "Cuenta añadida a cliente nuevo.\n";
+            s += "Nombre cuenta: " + clientes[clientes.length-1].getNombre() + ".\n";
+            s += "ID Cuenta: " + clientes[clientes.length-1].getCuentas()[0].getID() + ".\n";
+            s += "Saldo: " + clientes[clientes.length-1].getCuentas()[0].getSaldo() + ".\n";
+            
+        }
+        return s;
+    }
+    
+    /**
+     * Método que dada una cuenta muestra información sobre ésta.
+     * 
+     * @param id
+     * @return
+     * @throws ExcepcionCuentaNoExiste 
+     */
+    public String apartado2(int id) throws ExcepcionCuentaNoExiste {
+        for (Cliente cliente : clientes) {
+            for (int j = 0; j < clientes[j].getCuentas().length; j++) {
+                if (cliente.getCuenta(j).getID() == id) {
+                    return "Nombre: " + cliente.getNombre() + "\nSaldo: " + 
+                            cliente.getCuenta(j).getSaldo() + "\nCuenta: " + 
+                            cliente.getCuenta(j).getID();
                 }
             }
-            clientes[posCli] = cli;
-            posCli++;
-        } catch (ExceptionClienteExistente e) {
-            System.out.println("\nERROR: Cliente Existente!");
         }
-        return cli;
-
+        throw new ExcepcionCuentaNoExiste();
     }
-
-    /*Método para imprimir los clientes existentes por pantalla*/
-    public void clientesToString() {
-
+    
+    /**
+     * Método que dado un cliente muestra todas las cuentas asociadas.
+     * 
+     * @param nombreCliente
+     * @return
+     * @throws ExcepcionClienteNoEncontrado 
+     */
+    public String apartado3(String nombreCliente) throws ExcepcionClienteNoEncontrado {
+        String salida = "";
+        int indice;
+        indice = buscarCliente(nombreCliente);
+        for (int i = 0; i < clientes[indice].getCuentas().length; i++) {
+            salida += "Cuenta " + clientes[indice].getCuenta(i).getID() + ": Saldo: "
+                    + clientes[indice].getCuenta(i).getSaldo() + "\n";
+        }
+        return salida;
+    }
+    
+    /**
+     * Método para añadir o quitar saldo de una cuenta.
+     * 
+     * @param ID
+     * @param sal
+     * @param añadir
+     * @throws ExceptionTransferencia
+     * @throws ExcepcionCuentaNoEncontrada 
+     */
+    public String apartado4y5(int ID, int sal, boolean añadir) throws ExceptionTransferencia, ExcepcionCuentaNoEncontrada{
+        if(sal<=0){
+            throw new ExceptionTransferencia();
+        }
         for (int i = 0; i < clientes.length; i++) {
-            System.out.println(clientes[i]);
+            try {
+                int ind=buscarCuenta(ID,i);
+                if(!añadir){
+                    sal=-sal;
+                }
+                clientes[i].getCuenta(ind).actualizarSaldo(sal);
+                return "El saldo de la cuenta " + ID + " es: " + clientes[i].getCuenta(ind).getSaldo();
+            } catch (ExcepcionCuentaNoEncontrada ex) {}
         }
+       //throw new ExcepcionCuentaNoEncontrada();
+       return "falla aqui.";
+    }
+    
+    /**
+     * Método dada una cuenta borra esta, y si el cliente se queda sin cuentas
+     * borra el cliente también.
+     * 
+     * @param nombreCliente
+     * @param idCuenta
+     * @return 
+     * @throws ExcepcionClienteNoEncontrado
+     * @throws ExcepcionCuentaNoEncontrada 
+     */
+    public String apartado6(String nombreCliente, int idCuenta) throws ExcepcionClienteNoEncontrado, ExcepcionCuentaNoEncontrada {
+        int indice = buscarCliente(nombreCliente);
+        int indiceCuenta = buscarCuenta(idCuenta, indice);
+        clientes[indice].redimensionarArrayCuentas(true, indiceCuenta);
+        if (clientes[indice].getCuentas().length == 0) {
+            redimensionarArrayCli(true, indice);
+            return "El cliente ha sido borrado con éxito";
+        }
+        return "La cuenta ha sido borrado con éxito";
+    }
+    
+    /**
+     * Método que genera una ID única nueva para una cuenta.
+     * 
+     * @return 
+     */
+    private int generadorID() {
+        int ID = 0;
+        for (Cliente cliente : clientes) {
+            for (Cuenta cuenta : cliente.getCuentas()) {
+                if(ID<cuenta.getID()){
+                    ID=cuenta.getID();
+                }
+            }
+        }
+        return ID+1;
     }
 
-    /*Método para redimensionar el array de clientes*/
+    /**
+     * Método utilizado para buscar un cliente en concreto, si no lo encuentra
+     * lanza una excepción.
+     * 
+     * @param nombreCliente
+     * @return
+     * @throws ExcepcionClienteNoEncontrado 
+     */
+    private int buscarCliente(String nombreCliente) throws ExcepcionClienteNoEncontrado {
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i].getNombre().equals(nombreCliente)) {
+                return i;
+            }
+        }
+        throw new ExcepcionClienteNoEncontrado();
+    }
+
+    
+    /**
+     * Método utilizado para buscar un cliente en concreto, si no la encuentra
+     * lanza una excepción.
+     * 
+     * @param id
+     * @param indiceCliente
+     * @return
+     * @throws ExcepcionCuentaNoEncontrada 
+     */
+    private int buscarCuenta(int id, int indiceCliente) throws ExcepcionCuentaNoEncontrada {
+        for (int i = 0; i < clientes[indiceCliente].getCuentas().length; i++) {
+            if (clientes[indiceCliente].getCuentas()[i].getID() == id) {
+                return i;
+            }
+        }
+        throw new ExcepcionCuentaNoEncontrada();
+    }
+
+    /**
+    * Método que permite realizar dos acciones sobre el array clientes:
+    *   1.- Eliminar una posición marcada por índice del array.
+    *   2.- Añade tantas posiciones como marca el índice al array.
+    * 
+    * @param eliminar
+    * @param indice 
+    */
     private void redimensionarArrayCli(boolean eliminar, int indice) {
 
         if (eliminar) {
-
             Cliente[] temporal = new Cliente[this.clientes.length - 1];
 
             for (int i = 0; i < temporal.length; i++) {
-                if (indice < i) {
+                if (indice > i) {
                     temporal[i] = this.clientes[i];
                 } else {
                     temporal[i] = this.clientes[i + 1];
                 }
             }
-
             this.clientes = temporal;
         } else {
             Cliente[] temporal = new Cliente[this.clientes.length + indice];
@@ -112,9 +207,7 @@ public class MetodosyDatos {
             for (int i = 0; i < this.clientes.length; i++) {
                 temporal[i] = this.clientes[i];
             }
-
             this.clientes = temporal;
         }
-
     }
 }
